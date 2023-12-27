034be8ebe43a0188e740ca425726e66f5d8384bd
Oksana Dvornitska <userdvornicka@gmail.com>
2023-12-27 13:53:58 +0300

    added SwaggerConfig

--- src/main/java/oksana/dvornitska/JavaWeb.java
+++ src/main/java/oksana/dvornitska/JavaWeb.java
@@ -1,11 +1,12 @@
 package oksana.dvornitska;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Friend Locator API", version = "1.0"))
 public class JavaWeb {
     public static void main(String[] args) {
         SpringApplication.run(JavaWeb.class, args);
